0ad40644fd4699570f168e15d9e7f760dd3b4ce6
mohammed-yasmin <mohammed.yasmin@hcl.com>
2024-01-29 15:54:02 +0530

    Promise.any() failure case

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const p1= new Promise((resolve, reject)=>{
-setTimeout(()=>resolve("P1 resolved"),3000);
+setTimeout(()=>reject("P1 failed"),3000);
 });
 
 const p2= new Promise((resolve, reject)=>{
-    setTimeout(()=>resolve("P2 resolved"),2000);
+    setTimeout(()=>reject("P2 failed"),2000);
     });
     
 
@@ -13,5 +13,8 @@ const p3= new Promise((resolve, reject)=>{
         
         
 Promise.any([p1,p2,p3])
-.then(res => console.log(res)).catch((err)=> console.error(err))
-;
+.then(res => console.log(res)).catch((err)=> {
+    console.error(err);
+    console.error(err.errors);
+}
+);
--- readme.md
+++ readme.md
@@ -8,3 +8,18 @@ Promise.any() will wait for first settled success
 -----------------------
 
 Promise.all([p1,p2,p3]) failed case
+index.js:17 AggregateError: All promises were rejected
+index.js:18 
+(3) ['P1 failed', 'P2 failed', 'P3 failed']
+0
+: 
+"P1 failed"
+1
+: 
+"P2 failed"
+2
+: 
+"P3 failed"
+length
+: 
+3
